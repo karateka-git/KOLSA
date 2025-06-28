e44daf1a0e5af5aeaf57c73dcdf69ec44d8a12b4
compadre <zvvlad2@gmail.com>
2025-06-28 16:16:47 +0600

    VE: refactor MainActivity.onCreate with ViewBinding

--- app/src/main/java/com/example/kolsa/MainActivity.kt
+++ app/src/main/java/com/example/kolsa/MainActivity.kt
@@ -15,8 +15,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
