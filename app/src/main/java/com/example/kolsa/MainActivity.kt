67f5ebce4e188dace0a8cedd168a8b41ea1c9427
compadre <zvvlad2@gmail.com>
2025-06-28 16:15:01 +0600

    VE: add ViewBinding

--- app/src/main/java/com/example/kolsa/MainActivity.kt
+++ app/src/main/java/com/example/kolsa/MainActivity.kt
@@ -5,8 +5,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.kolsa.databinding.ActivityMainBinding
+import com.example.kolsa.presentation.util.ext.onDestroyNullable
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding by onDestroyNullable<ActivityMainBinding>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
