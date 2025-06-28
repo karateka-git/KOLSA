a2d7765a4abe6675d9ea82d522187b85eeaef50c
compadre <zvvlad2@gmail.com>
2025-06-29 00:54:48 +0600

    VE: fix system back button not navigate to back

--- app/src/main/java/com/example/kolsa/presentation/MainActivity.kt
+++ app/src/main/java/com/example/kolsa/presentation/MainActivity.kt
@@ -1,17 +1,16 @@
 package com.example.kolsa.presentation
 
 import android.os.Bundle
+import androidx.activity.OnBackPressedCallback
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.example.kolsa.databinding.ActivityMainBinding
 import com.example.kolsa.presentation.util.ext.onDestroyNullable
 
-class MainActivity : AppCompatActivity() {
 
+class MainActivity : AppCompatActivity() {
     private var binding by onDestroyNullable<ActivityMainBinding>()
     private lateinit var navController: NavController
 
@@ -21,10 +20,14 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         navController = binding.navHostFragment.getFragment<NavHostFragment>().navController
-        ViewCompat.setOnApplyWindowInsetsListener(binding.navHostFragment) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val callback: OnBackPressedCallback =
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    if (navController.popBackStack().not())
+                        finish()
+                }
+            }
+        onBackPressedDispatcher.addCallback(this, callback)
     }
 }
